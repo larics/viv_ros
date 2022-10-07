80e4402212cc39771e490c5693179805984992cb
ivatavuk <ivo.vatavuk@fer.hr>
2022-10-07 18:57:41 +0200

    Refactor viv_base code

--- viv_base/include/viv_hardware.h
+++ viv_base/include/viv_hardware.h
@@ -10,47 +10,47 @@
 #include "std_msgs/Float64MultiArray.h"
 
 namespace viv_base {
-    class VivHardware : public hardware_interface::RobotHW
-    {
-        public:
+class VivHardware : public hardware_interface::RobotHW
+{
+public:
 
-            VivHardware(ros::NodeHandle nh, ros::NodeHandle private_nh, double target_control_freq);
+  VivHardware(ros::NodeHandle nh, ros::NodeHandle private_nh);
 
-            void writeCommandsToHardware();
+  void writeCommandsToHardware();
 
-        private:
+private:
 
-            void registerControlInterfaces();
-            double radps_to_rpm(double vel_radps);
+  void registerControlInterfaces();
+  static double radps_to_rpm(double vel_radps);
 
-            ros::NodeHandle nh_, private_nh_;
+  ros::NodeHandle nh_, private_nh_;
 
-            // ROS Control interfaces
-            hardware_interface::VelocityJointInterface velocity_joint_interface_;
+  // ROS Control interfaces
+  hardware_interface::VelocityJointInterface velocity_joint_interface_;
 
-            // ROS Parameters
-            double wheel_diameter_, max_accel_, max_speed_;
+  // ROS Parameters
+  double wheel_diameter_, max_accel_, max_speed_;
 
-            double polling_timeout_;
+  double polling_timeout_;
 
-            /*
-             * Joint structure that is hooked to ros_control's InterfaceManager, to allow control via diff_drive_controller
-             */
-            struct Joint
-            {
-                double position;
-                double position_offset;
-                double velocity;
-                double effort;
-                double velocity_command;
+  /*
+    * Joint structure that is hooked to ros_control's InterfaceManager, to allow control via diff_drive_controller
+    */
+  struct Joint
+  {
+    double position;
+    double position_offset;
+    double velocity;
+    double effort;
+    double velocity_command;
 
-                Joint() :
-                position(0), velocity(0), effort(0), velocity_command(0)
-                { }
-            } joints_[4];
+    Joint() :
+    position(0), velocity(0), effort(0), velocity_command(0)
+    { }
+  } joints_[4];
 
-            ros::Publisher velocity_command_pub_;
-    };
+  ros::Publisher velocity_command_pub_;
+};
 
 } // namespace viv_base
 #endif  // VIV_HARDWARE_H
