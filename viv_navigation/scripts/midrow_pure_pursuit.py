2d09566bdb52871f665a9f8f976f8dc22f2f0fc2
ivatavuk <ivo.vatavuk@fer.hr>
2023-02-24 18:27:06 +0100

    Add initial states to midrow_pure_pursuit

--- viv_navigation/scripts/midrow_pure_pursuit.py
+++ viv_navigation/scripts/midrow_pure_pursuit.py
@@ -3,7 +3,7 @@ import rospy
 import time
 
 from geometry_msgs.msg import Twist
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PointStamped, PoseStamped, Point
 
 class midrow_pure_pursuit:
   def __init__(self):
@@ -18,23 +18,38 @@ class midrow_pure_pursuit:
     self.cmd_vel_pub = rospy.Publisher(self.cmd_vel_topic, Twist, queue_size=1)
     self.front_distance_to_end = 9999
     self.min_front_vel = 0.05
+
+    self.move_base_goal_pub = rospy.Publisher("/erl_husky/move_base_simple/goal", PoseStamped, queue_size=1)
     
+    self.reached_end_of_row = False
+
+    self.state = "MIDROW_NAV"
+    self.pure_pursuit_point = Point()
+    self.new_pure_pursuit_point_rec = False
+
   def midline_callback(self, msg):
-    cmd_vel = Twist()
-    if(float(msg.point.x) < self.front_distance_to_end):
-      cmd_vel.linear.x = self.KP_linear * float(msg.point.x)
-    else:
-      cmd_vel.linear.x = self.KP_linear * self.front_distance_to_end
-    cmd_vel.angular.z = self.KP_angular * msg.point.y
-
-    if(cmd_vel.linear.x > self.min_front_vel):
-      self.cmd_vel_pub.publish(cmd_vel)
-
+    self.pure_pursuit_point = msg.point
+    self.new_pure_pursuit_point_rec = True
+    
   def this_row_enter_callback(self, msg):
     self.front_distance_to_end = float(msg.point.x)
 
   def loop(self):
     while not rospy.is_shutdown():
+      if(self.state == "MIDROW_NAV"):
+        if(not self.new_pure_pursuit_point_rec):
+          continue
+        self.new_pure_pursuit_point_rec = False
+        cmd_vel = Twist()
+        if(self.pure_pursuit_point.x < self.front_distance_to_end):
+          cmd_vel.linear.x = self.KP_linear * self.pure_pursuit_point.x
+        else:
+          cmd_vel.linear.x = self.KP_linear * self.front_distance_to_end
+        cmd_vel.angular.z = self.KP_angular * self.pure_pursuit_point.y
+
+        if(cmd_vel.linear.x > self.min_front_vel):
+          self.cmd_vel_pub.publish(cmd_vel)
+
       self.rate.sleep()
   
 if __name__ == '__main__':
