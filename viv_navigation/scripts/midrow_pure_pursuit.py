7302040053d086be367709768fc455bf6fac1d42
ivatavuk <ivo.vatavuk@fer.hr>
2023-02-24 18:36:24 +0100

    Add initial cartographer state logic

--- viv_navigation/scripts/midrow_pure_pursuit.py
+++ viv_navigation/scripts/midrow_pure_pursuit.py
@@ -34,21 +34,31 @@ class midrow_pure_pursuit:
   def this_row_enter_callback(self, msg):
     self.front_distance_to_end = float(msg.point.x)
 
+  def midrow_nav_loop(self):
+    if(not self.new_pure_pursuit_point_rec):
+      return
+    self.new_pure_pursuit_point_rec = False
+    cmd_vel = Twist()
+    if(self.pure_pursuit_point.x < self.front_distance_to_end):
+      cmd_vel.linear.x = self.KP_linear * self.pure_pursuit_point.x
+    else:
+      cmd_vel.linear.x = self.KP_linear * self.front_distance_to_end
+    cmd_vel.angular.z = self.KP_angular * self.pure_pursuit_point.y
+
+    if(cmd_vel.linear.x > self.min_front_vel):
+      self.cmd_vel_pub.publish(cmd_vel)
+    elif(self.front_distance_to_end < 1.0):
+      self.state = "CARTOGRAPHER_NAV"
+
+  def cartographer_nav_loop(self):
+    print("Cartographer oh yeaaa!")
+
   def loop(self):
     while not rospy.is_shutdown():
       if(self.state == "MIDROW_NAV"):
-        if(not self.new_pure_pursuit_point_rec):
-          continue
-        self.new_pure_pursuit_point_rec = False
-        cmd_vel = Twist()
-        if(self.pure_pursuit_point.x < self.front_distance_to_end):
-          cmd_vel.linear.x = self.KP_linear * self.pure_pursuit_point.x
-        else:
-          cmd_vel.linear.x = self.KP_linear * self.front_distance_to_end
-        cmd_vel.angular.z = self.KP_angular * self.pure_pursuit_point.y
-
-        if(cmd_vel.linear.x > self.min_front_vel):
-          self.cmd_vel_pub.publish(cmd_vel)
+        self.midrow_nav_loop()
+      if(self.state == "CARTOGRAPHER_NAV"):
+        self.cartographer_nav_loop()
 
       self.rate.sleep()
   
