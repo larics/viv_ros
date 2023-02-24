a1074a6a72d517b9d3557f33e10d9d3a81ef6b70
ivatavuk <ivo.vatavuk@fer.hr>
2023-02-24 18:10:57 +0100

    Use distance to end row for midrow navigation

--- viv_navigation/scripts/midrow_pure_pursuit.py
+++ viv_navigation/scripts/midrow_pure_pursuit.py
@@ -12,14 +12,26 @@ class midrow_pure_pursuit:
     self.KP_linear = 0.1
     self.KP_angular = 0.5
     rospy.Subscriber("/pure_pursuit_point", PointStamped, self.midline_callback)
-    self.cmd_vel_pub = rospy.Publisher("/viv/spraying_mpc/cmd_vel", Twist, queue_size=1)
+    rospy.Subscriber("/this_row_enter_point", PointStamped, self.this_row_enter_callback)
+    #self.cmd_vel_topic = "/viv/spraying_mpc/cmd_vel"
+    self.cmd_vel_topic = "/viv/viv_velocity_controller/cmd_vel"
+    self.cmd_vel_pub = rospy.Publisher(self.cmd_vel_topic, Twist, queue_size=1)
+    self.front_distance_to_end = 9999
+    self.min_front_vel = 0.05
+    
   def midline_callback(self, msg):
     cmd_vel = Twist()
-    cmd_vel.linear.x = self.KP_linear * float(msg.point.x)
+    if(float(msg.point.x) < self.front_distance_to_end):
+      cmd_vel.linear.x = self.KP_linear * float(msg.point.x)
+    else:
+      cmd_vel.linear.x = self.KP_linear * self.front_distance_to_end
     cmd_vel.angular.z = self.KP_angular * msg.point.y
 
-    self.cmd_vel_pub.publish(cmd_vel)
+    if(cmd_vel.linear.x > self.min_front_vel):
+      self.cmd_vel_pub.publish(cmd_vel)
 
+  def this_row_enter_callback(self, msg):
+    self.front_distance_to_end = float(msg.point.x)
 
   def loop(self):
     while not rospy.is_shutdown():
